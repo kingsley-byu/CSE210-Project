73b84d0b7c2b560edcf595e322f0f535c8d6ba4d
kingsley-byu <osaruyiosas@gmail.com>
2026-03-13 22:21:08 +0100

    completed the Resume assignment

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
-    }
-}
+        Job job1 = new Job();
+        job1._company = "Microsoft";
+        job1._jobTitle = "Software Developer";
+        job1._startYear = 2022;
+        job1._endYear = 2024;
+
+        Job job2 = new Job();
+        job2._company = "Amazon";
+        job2._jobTitle = "Cloud Engineer";
+        job2._startYear = 2024;
+        job2._endYear = 2026;
+
+        Resume myResume = new Resume();
+        myResume._name = "Kingsley Osawaru Osaruyi";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
+
+
+    } 
+}  
