d70a4b7ca58b23492b94263423cb13c074c8f52e
kingsley-byu <osaruyiosas@gmail.com>
2026-03-20 23:34:22 +0100

    initializes all 3 classes

---
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Scripture
+{
+    Reference _reference = new Reference();
+    List<Word> _words = new List<Word>();
+    {
+        
+    }
+    Scripture(Reference reference, string text);
+
+
+}
