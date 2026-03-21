07fcd6b617f3f9e13272f2307418cc4e3a15a2ee
kingsley-byu <osaruyiosas@gmail.com>
2026-03-21 10:52:39 +0100

    wrote the scripure class program and added the constructor, loop through and split text

--- week03/ScriptureMemorizer/Reference.cs
+++ week03/ScriptureMemorizer/Reference.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Data;
 public class Reference
 {
     private string _book;
@@ -6,13 +7,32 @@ public class Reference
     private int _verse;
     private int _endVerse;
 
-    public Reference()
+    public Reference(string book, int chapter, int verse)
     {
-        
+      _book = book;
+      _chapter = chapter;
+      _verse = verse;
+      _endVerse = 0;  
+    }
+    public Reference(string book, int chapter, int startVerse, int endVerse)
+    {
+       _book = book;
+       _chapter = chapter;
+       _verse = startVerse;
+       _endVerse = endVerse;
+
     }
 
     public string GetDisplayText()
     {
-        
+        if (_endVerse == 0)
+        {
+
+            return $"{_book} {_chapter}:{_verse}";
+        }
+        else
+        {
+            return $"{_book} {_chapter}:{_verse}-{_endVerse}";
+        }
     }
 }
--- week03/ScriptureMemorizer/Scripture.cs
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -1,13 +1,35 @@
 using System;
+using System.Collections.Generic;
 
 public class Scripture
 {
-    Reference _reference = new Reference();
+    private Reference _reference;
     List<Word> _words = new List<Word>();
+    
+    public Scripture(Reference reference, string text)
+    {
+        _reference = reference;
+        string [] parts = text.Split(" ");
+        
+        foreach (string word in parts)
+        {
+            Word w1 = new Word(word);
+            _words.Add(w1);
+        }
+
+    }
+    
+    public void HideRandomWords(int numberToHide)
     {
         
     }
-    Scripture(Reference reference, string text);
+    public string GetDisplayText()
+    {
+        
+    }
 
-
+    public bool IsCompletelyHidden()
+    {
+        
+    }
 }
