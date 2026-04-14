ef73495f3a64626b77564ee52537d9ae1c1d50ab
kingsley-byu <osaruyiosas@gmail.com>
2026-04-14 12:39:01 +0100

    started the week 7 project

---
+++ week07/ExerciseTracking/Activity.cs
@@ -0,0 +1,30 @@
+public abstract class Activity
+{
+    private string _date;
+    private int _minutes;
+
+    public Activity(string date, int minutes)
+    {
+        _date = date;
+        _minutes = minutes;
+    }
+
+    protected string GetDate()
+    {
+        return _date;
+    }
+
+    protected int GetMinutes()
+    {
+        return _minutes;
+    }
+
+    public abstract double GetDistance();
+    public abstract double GetSpeed();
+    public abstract double GetPace();
+
+    public virtual string GetSummary()
+    {
+        return $"{GetDate()} {GetType().Name} ({GetMinutes()} min) - Distance: {GetDistance():0.0} miles, Speed: {GetSpeed():0.0} mph, Pace: {GetPace():0.0} min per mile";
+    }
+}
